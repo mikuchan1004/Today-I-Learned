5890871cacef902d49e0a3a39f6f3204473daa48
김상우 <kimsangwoo22@gmail.com>
2026-07-29 12:12:24 +0900

    파이썬 내장함수, 2차원 리스트 코드 추가

--- workspace_python/04_list.py
+++ workspace_python/04_list.py
@@ -42,8 +42,11 @@ b = [9, 10]
 a.append(b)
 print (a)
 
+print('-='*30)
 c = [654, 156, 964, 15, 35]
 c.sort() # 오름차순
+print(c)
+c.sort(reverse=True)
 print(c)
 
 c = c[::-1]
@@ -57,3 +60,102 @@ print(c)
 
 c.insert(10, 200) # index를 벗어나면 걍 끝에 
 print(c)
+
+c.extend([1, 2]) # 리스트를 합쳐준다
+print(c)
+
+a = [1, 2, 3, 4, 2]
+a.remove(2) # 처음 만나는 값을 찾아서 지워준다
+print(a)
+
+if 5 in a :         
+    a.remove(5) # 없는 값은 에러난다
+
+a = [1, 2, 3, 4, 2, 4]
+b = a.index(2)
+print(b)
+# b = a.index(5) # 없는 값은 에러
+
+c = a.count(4)
+print(c)
+
+a.reverse()
+print(a)
+
+a.clear()
+print(a)
+a = []
+
+a = [1, 2, 3]
+print (a[len(a):])
+print(a[3:])
+
+# a[3:] = 4
+a[3:] = [4, 5, 6]
+print(a)
+
+a = [1, 2, 3, 4, 5]
+b = a
+print(b)
+b[2] = 30
+print(b)
+print(a)
+b = a.copy()
+b[2] = 30
+print(b)
+print(a)
+
+print('-=' * 30)
+a = [1, 2, 3, 4, 5]
+b = a.copy()
+b[2] = 30
+print(b)
+print(a)
+
+c = (1, 2)
+a = c[0]
+b = c[1]
+
+a, b = (1,2)
+
+a = [10, 20, 30]
+for i in a:
+    print(i)
+
+# 이번 턴의 index와 value를 한번에 뽑아 줌
+for index, value in enumerate(a) :
+    print(index, value)
+
+# 시작 인덱스 지정 가능
+for index, value in enumerate(a, start=100) :
+    print(index, value)
+
+a = [7, 3, 5, 8, 4]
+# 가장 큰 수 찾기
+a.sort(reverse=True) # sort는 기본적으로 오름차순인데, reverse=True를 하면 내림차순으로!
+print(a[0]) # 내림차순으로 하면 리스트의 첫 번째 요소가 가장 큰 수가 된다.
+
+a = [i for i in range(10)] # 0부터 9까지 숫자를 생성하여 리스트 생성
+print(a)
+
+a = [i for i in range(10) if i % 2 == 0] # 0~9 숫자 중 2의 배수(짝수)로
+print(a) # 리스트 생성
+
+a = [1.2 , 2.5 , 3.7 , 4.6 , -3.5]
+for i, value in enumerate(a) :
+    a[i] = int(value)
+print(a)
+
+# 두 번째 반복되는 것을 하나씩 꺼내서 
+# 첫 번째 함수에 넣고 
+# 결과를 배열로 만들어준다.
+a = list(map(int, a))
+print(a)
+
+a = [
+    [10, 20],
+    [30, 40],
+    [50, 60]
+]
+print(len(a))
+print(a[1][0])
