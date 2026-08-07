9cbaf877cd69224f79e2ca500fb859d169d931a0
김상우 <kimsangwoo22@gmail.com>
2026-08-07 13:26:18 +0900

    Update Script.sql

--- workspace_sql/Script.sql
+++ workspace_sql/Script.sql
@@ -137,7 +137,6 @@ where comm is null
 select * from emp
 where comm is not null
 
-
 -- order by
 
 select * from emp
@@ -194,5 +193,229 @@ where
 	(deptno = 20 or deptno = 30) 
 	and sal between 2000 and 3000
 order by sal asc, ename desc;
+
+-- 집계 함수
+
+select count(ename) from emp;
+select count(mgr) from emp;
+select count(comm) from emp; -- null은 개수에서 제외 
+select count(*) from emp;
+
+select max(sal) from emp;
+select min(sal) from emp;
+
+select sum(sal) from emp;
+select avg(sal) from emp;
+
+select length(ename) , ename from emp;
+
+-- select count(*), ename from emp; 원하는 결과가 나오지 않기때문에 쓰지 않는것이 좋다.
+
+select * from emp 
+where length(ename) = 4;
+
+-- 대상의 몇 번재부터 몇 개를 잘라오기
+
+select substring(ename, 2, 3 ), ename from emp;
+select substr(ename, 2, 3), ename from emp;
+
+-- 전부 교체
+
+select replace (ename, 'A' ,'에이'), ename from emp;
+
+-- 대상의 자리수를 맞춰주고 남으면 채워준다
+
+select lpad(ename, 10, '#') from emp;
+select lpad(ename, 3, '#') from emp;
+
+select rpad(ename, 10, '#') from emp;
+
+select lpad(sal, 10, '0') from emp;
+select lpad(ename, 10, ' ') from emp;
+
+select trim(' a b c  ')
+
+select concat(ename, job) from emp;
+select concat(ename, ' ' , job) from emp;
+
+-- 오라클에서 합치기 ename || job으로 사용 가능
+
+select concat_ws('-' , ename, job, empno) from emp;
+
+-- 반올림
+
+select round(3.14);
+select round(3.145, 2);
+
+-- 올림
+
+select ceil(3.14);
+select ceil(-3.14);
+
+-- 내림
+
+select floor(3.14);
+select floor(-3.14);
+
+-- 버림
+
+select truncate(-3.14, 1);
+
+-- 나머지 
+
+select mod(10, 3);
+
+-- 현재 시간
+
+select now();
+select sysdate();
+
+-- 날짜 출력 양식 지정
+
+select date_format(now(), '%Y년 %M월 %D일 %H시 %i분 %s초');
+
+-- 문자를 날짜 형으로 변환
+
+select str_to_date('2026-08-07', '%Y-%m-%d');
+
+select ifnull(comm, 0), comm from emp;
+select coalesce(comm, 0), comm from emp;
+
+select sal * 12 + comm from emp;
+select sal * 12 + ifnull(comm, 0) from emp;
+
+-- 문제
+-- ename 앞 두 글자만 출력
+
+select substr(ename, 1, 2) , ename from emp;
+
+-- ename의 앞 두글자만 원본 그대로 추력하고 
+-- 4개의 *을 붙여서 출력 
+
+select substr(ename, 1, 2) , rpad('' , 4, '*') from emp;
+
+select rpad(substring(ename, 1, 2), 6, '*') from emp;
+
+-- ename의 앞 두 글자만 원본 그대로 출력하고 
+-- 나머지 이름 만큼의 * 출력
+
+select rpad(substring(ename, 1, 2), length(ename), '*') from emp; 
+
+-- case문 
+
+select * from emp;
+
+select 
+	job, sal,
+	case job 
+		when 'CLERK' then sal * 1.05
+		when 'SALESMAN' then sal * 1.03
+		else sal 
+	end as upsal
+from emp;
+
+select 
+	job, sal,
+	case 
+		when job = 'CLERK' then sal * 1.05
+		when job = 'SALESMAN' then sal * 1.03
+		else sal 
+	end as upsal
+from emp;
+
+select 
+	sal, comm,
+	case
+		when comm is null then 0
+		else comm
+	end
+from emp;
+
+-- GROUP BY절
+
+select deptno from emp
+group by deptno;
+
+select 
+	deptno, count(*), sum(sal)
+from emp
+group by deptno;
+
+select deptno, job, count(*)
+from emp
+group by deptno, job;
+
+select deptno, job
+from emp
+where deptno = 10
+group by deptno, job;
+
+select deptno, job
+from emp
+where deptno = 10
+group by deptno, job
+order by job;
+
+select avg(sal) from emp;
+
+-- select 
+-- 		avg(sal)
+-- from emp
+-- where sal >= avg(sal);
+
+select 
+	avg(sal) as avg_sal,
+	deptno,
+	job
+from emp 
+group by deptno, job
+having avg(sal) >= 2000;
+
+-- where 조건을 having에 적을 수 있지만
+-- 통상 group by와 관련된 것만 적는다.
+
+
+-- 직업 별로 연봉 1000 이상인 사람이 3명 이상인 경우만 출력
+
+select
+	job,count(*) cnt
+from emp
+where sal >= 1000
+group by job
+having count(*) >= 3;
+
+/* 5 */select job, 1 as num
+/* 1 */from emp
+/* 2 */where sal > 1000
+/* 3 */group by job
+/* 4 */having count(*) >= 3
+/* 6 */order by job desc, num
+
+select * from emp where deptno = 10
+union
+select * from emp where deptno = 10;
+
+select * from emp where deptno = 10
+union all
+select * from emp where deptno = 10;
+
+-- 서브 쿼리문
+
+select * from emp
+where sal > 1250;
+
+-- 'WARD'의 연봉만 출력
+select sal
+from emp
+where ename = 'WARD';
+
+select * from emp
+where sal > (select sal
+			from emp 
+			where ename = 'WARD');
+
+select * from emp
+where sal > (select avg(sal) from emp);
+
 
 
