0e1b958727ffedb1adaf3c3749d57080f37e3f4d
김상우 <kimsangwoo22@gmail.com>
2026-08-13 16:46:14 +0900

    파이썬 가상환경(todos) 폴더 업로드.

---
+++ workspace_python/todos/01_router/api.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from todo import todo_router #type:ignore
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+@app.get("/")
+async def welcome() -> dict:
+    return {
+        "message" : "Hello World"
+    }
+
+app.include_router(todo_router)
+
+print(1, __name__)
+
+if __name__ == "__main__":
+    print('api.py 파일 직접 실행')
+
+    import uvicorn
+    uvicorn.run("api:app" , port=8000, reload=True)
