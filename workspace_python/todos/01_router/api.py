4ae3d0775f278c1b959c3f51b43cbd39d243fc5c
김상우 <kimsangwoo22@gmail.com>
2026-08-14 12:32:29 +0900

    api.py에 quiz_00.quiz1 모듈에서 todo_router 추가 및 ajax.html에 숫자 입력 폼 추가, quiz1.py 파일 생성

--- workspace_python/todos/01_router/api.py
+++ workspace_python/todos/01_router/api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from todo import todo_router #type:ignore
+from quiz_00.quiz1 import todo_router
 
 # 크로스 도메인 CORS 해결 코드
 app = FastAPI()
---
+++ workspace_python/todos/quiz_00/quiz1.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Request
+
+todo_router = APIRouter()
+
+
+# 문제 1.
+# 브라우저에서 
+# - input에 몇 단 입력
+# 파이썬에서 
+# - 해당 구구단의 단 출력 
+
+@todo_router.post("/todo/param3")
+async def todoParam(req:Request):
+    data = await req.form()
+    number = data.get('number')
+
+    # 1. 입력값이 유효한 숫자인지 확인 
+    if number and number.isdigit():
+        num = int(number)
+
+        # 2. 구구단 계산 결과를 리스트로 생성
+        gugudan_list = [f"{num} x {i} = {num * i}" for i in range(1,10)]
+    
+        return {
+            "number" : num,
+            "gugudan" : gugudan_list
+        }
