79eb47e8b839b9f419a8ce259fd401ce883cc42f
김상우 <kimsangwoo22@gmail.com>
2026-08-24 15:22:00 +0900

    FastAPI 데이터베이스 연결 연습을 위한 코드, HTML 추가

---
+++ workspace_python/03_database/DTO/EmpDTO.py
@@ -0,0 +1,19 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+
+class Emp3 (SQLModel, table=True):
+    # 없으면 클래스명이 테이블명이 된다.
+    # __tablename__ = "emp"
+
+    empno : int = Field(primary_key=True) 
+    ename : str 
+    job : str
+    # mgr : int | None = None
+    mgr : Optional[int] = None
+    hiredate : str
+    sal : float
+    # comm : float | None = None
+    comm : Optional[float] = None
+    deptno : int = Field(
+        foreign_key='dept3.deptno'
+    )
