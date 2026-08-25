6abc5e8906b659fa4c14974e7d4e17c225b03999
김상우 <kimsangwoo22@gmail.com>
2026-08-25 14:11:10 +0900

    데이터베이스 활용 CRUD 코드 완성본커밋

--- workspace_python/03_database/03_crud.py
+++ workspace_python/03_database/03_crud.py
@@ -3,19 +3,22 @@ from fastapi import FastAPI, Depends, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
 from sqlalchemy import text
-from typing import Optional # 빈 칸 허용을 위해 추가
 
+# SQLModel.metadata에 등록 시킨다
+# 그러면 crate_all로 테이블이 없으면 만들어 준다
+# Emp3(SQLModel, table=True) 이것 둘 다 적으면 자동으로 만들어준다.
 from DTO.EmpDTO import Emp3
 from DTO.DeptDTO import Dept3
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates/')
 
+
 DATABASE_URL = 'mysql+pymysql://root:human1234$@127.0.0.1:3306/human'
 engine = create_engine(DATABASE_URL, echo=True)
 
 def get_session():
-    with Session(engine) as session:
+    with Session(engine) as session : 
         yield session
         session.commit()
 
@@ -23,105 +26,92 @@ def get_session():
 def on_startup() :
     SQLModel.metadata.create_all(engine)
 
-# EMP 테이블 전체 조회 페이지 
 @app.get('/list')
-def emp_list(request: Request, session: Session = Depends(get_session)):
+def list(request: Request,session : Session = Depends(get_session)):
     print('/list 실행')
-    # DB에 직접 SELECT 쿼리를 날려서 목록을 가져옴 
-    sql = text('select * from emp3')
+    sql = text ('''
+        select * from emp3
+    ''')
     results = session.execute(sql).mappings().fetchall()
     return templates.TemplateResponse(request, 'list.html',{
         'emp_list' : results
-    }) 
+    })
 
-# EMP 테이블 추가 화면 이동 
+#추가 페이지로 이동 
 @app.get('/add')
-def add_emp(request:Request):
+def add_page(request: Request):
     print('/add 실행')
-    return templates.TemplateResponse(request, 'add.html')
+    return  templates.TemplateResponse(request, 'add.html')
 
-# EMP 테이블에 값을 추가하는 API
+# 테이블에 내용을 추가하는 API 
 @app.post('/api/add')
-def emp_add_form (
-    request: Request,
-    empno : int = Form(),
-    ename : str = Form(),
-    job : str = Form(),
-    mgr : Optional[int] = Form(None),
-    hiredate : str = Form(),
-    sal : float = Form(),
-    comm : Optional[float] = Form(None),
-    deptno : int = Form(),
-    session: Session = Depends(get_session)
-):
+def add_form(emp: Emp3 = Form(), session : Session = Depends(get_session)):
+    print('/api/add 실행')
+    print(emp)
     emp_list = []
-    try :
-        sql = text('''
-            insert into emp3 
-            (empno, ename, job, mgr, hiredate, sal, comm, deptno)
-            values (:empno, :ename, :job, :mgr, :hiredate, :sal, :comm, :deptno )
-        ''')
-
+    try:
+        sql = text ('''
+                insert into emp3
+                (empno, ename, job, mgr, hiredate, sal,  comm, deptno)
+                values (:empno, :ename, :job, :mgr, :hiredate, :sal, :comm, :deptno)
+            ''')
         session.execute(sql, {
-            "empno" : empno,
-            "ename" : ename,
-            "job" : job,
-            "mgr" : mgr,
-            "hiredate" : hiredate,
-            "sal" : sal,
-            "comm" : comm,
-            "deptno" : deptno
+            'empno' : emp.empno,
+            'ename' : emp.ename,   
+            'job' : emp.job,
+            'mgr' : emp.mgr,
+            'hiredate' : emp.hiredate,
+            'sal' : emp.sal,
+            'comm' : emp.comm,
+            'deptno' : emp.deptno
         })
-
         session.commit()
-    
     except Exception as e :
         print(e)
 
-#추가 완료 후 전체 페이지로 리다이렉트
-    return RedirectResponse(url='/list' , status_code=303)
+    #  추가 완료 후 전체 조회 페이지로 이동
+    return RedirectResponse(url = '/list' , status_code=303)
 
-# 상세 정보 조회 라우트
+# 상세 정보 조회
 @app.get('/detail/{empno}')
-def detail_page(empno:int, request: Request ,  session: Session = Depends(get_session)):
-    print('/datail/{empno} 실행,  empno:' , empno)
-    sql = text('''
-        select * from emp3
-        where empno = :empno
-         ''')
-    result = session.execute(sql, {"empno" : empno}).mappings().first()
-    return templates.TemplateResponse(request, 'detail.html',{
-            'request' : request,
-            'emp' : result
-        }) 
-
-# 수정 화면 이동 라우트
-@app.get('/update/{empno}')
-def update_page(empno:int, request: Request, session: Session = Depends(get_session)):
-    print('/update/{empno} 실행 , empno:', empno )
+def detail_page(empno:int, request:Request, session : Session = Depends(get_session)):
+    print('/detail/{empno} 실행' , empno)
     sql = text ('''
         select * from emp3
         where empno = :empno
     ''')
-    result = session.execute(sql, {"empno" : empno }).mappings().first()
-    return templates.TemplateResponse(request,'update.html', {
-        'request' : request,
-        'emp' : result
+    result =session.execute(sql, {'empno' : empno}).mappings().first()
+
+    return templates.TemplateResponse(request, 'detail.html',{
+        'emp' : result 
     })
 
-# 테이블 수정처리 API 라우트 
+#  수정 페이지 이동 라우트
+@app.get('/update/{empno}')
+def update_page (empno: int, request: Request, session:Session = Depends(get_session)):
+    print('/update/{empno} 실행' , empno)
+    sql = text ('''
+        select * from emp3
+        where empno = :empno
+    ''')
+    result = session.execute(sql, {'empno' : empno}).mappings().first()
+
+    return templates.TemplateResponse(request, 'update.html', {
+        'emp' : result 
+    })
+
+# 수정 처리 API 라우트
 @app.post('/api/update')
-def emp_update_form(
-    request: Request,  emp: Emp3 = Form(), session: Session = Depends(get_session)
-):
+def opi_update(emp: Emp3 = Form(), session : Session = Depends(get_session)):
+    print('/api/update 실행' , emp)
     try:
-        sql = text('''
+        sql = text ('''
             update emp3
             set 
+                empno = :empno,
                 ename = :ename,
                 job = :job,
-                mgr = :mgr,
-                hiredate = :hiredate,
+                mgr = :mgr, 
                 sal = :sal,
                 comm = :comm,
                 deptno = :deptno
@@ -130,24 +120,44 @@ def emp_update_form(
         ''')
         session.execute(sql, {
             'empno' : emp.empno,
-            'ename'  : emp.ename,
+            'ename' : emp.ename,
             'job' : emp.job,
             'mgr' : emp.mgr,
-            'hiredate' : emp.hiredate,
             'sal' : emp.sal,
             'comm' : emp.comm,
             'deptno' : emp.deptno
         })
-        
         session.commit()
 
-    except Exception as e :
+    except Exception as e:
         print(e)
 
-    # 수정 완료 후 전체 조회 페이지로 리다이렉트
-    return RedirectResponse(url='/list' , status_code=303)
+    #  수정  완료 후 전체 조회 페이지로 이동
+    return RedirectResponse(url = '/list' , status_code=303)
+
+# 삭제 처리 API 라우트 
+@app.get('/api/delete/{empno}')
+def api_delete(empno: int, session : Session = Depends(get_session)):
+    print('/api/delete 실행' , empno)
+    try:
+        sql = text ('''
+            delete from emp3
+            where empno = :empno
+        ''')
+        session.execute(sql, {'empno': empno})
+
+        session.commit()
+    except Exception as e:
+        print(e)
+
+    # 삭제 완료 후 전체 조회 페이지로
+    return RedirectResponse(url = '/list' , status_code=303)
 
 
+# 메인 모듈 실행 시 Uvicorn 개발 서버 시작
 if __name__ == "__main__":
+
     import uvicorn
-    uvicorn.run("03_crud:app" , port=8085, reload=True, host="0.0.0.0")
+    # uvicorn 서버 실행 (host: 0.0.0.0, port: 8000, 코드 변경 시 자동 재시작: reload=True)
+    # 포트 번호 변경 (사유 : 8000번 포트를 디스코드가 쳐먹...아니 선점해서 사용 불가.)
+    uvicorn.run("03_crud:app", port=8085, reload=True, host="0.0.0.0")
