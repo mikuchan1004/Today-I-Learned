bb32f90fb544bdde3c01b996ee9b5f6b95b6307c
김상우 <kimsangwoo22@gmail.com>
2026-08-25 14:10:13 +0900

    EmpDTO.py : 폼을 비워놔도 500에러가 안나게끔하는 코드 추가 (field_validator)

--- workspace_python/03_database/DTO/EmpDTO.py
+++ workspace_python/03_database/DTO/EmpDTO.py
@@ -1,7 +1,8 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from pydantic import field_validator
 
-class Emp3 (SQLModel, table=True):
+class Emp3 (SQLModel):
     # 없으면 클래스명이 테이블명이 된다.
     # __tablename__ = "emp"
 
@@ -21,3 +22,17 @@ class Emp3 (SQLModel, table=True):
     deptno : int = Field(
         foreign_key='dept3.deptno'
     )
+
+    @field_validator('comm' , mode='before')
+    @classmethod 
+    def empty_to_none(cls, value):
+        if value == '' :
+            return None 
+        else : 
+            return value
+
+    # @model_validator(mode='before')
+    # @classmethod
+    # def empty_to_none(cls, value):
+    #     print(value,  value if value != "" else None  )
+    #     return value if value != "" else None
