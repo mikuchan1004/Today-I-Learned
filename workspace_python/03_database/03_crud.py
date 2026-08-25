4a43d04833cb5285daddf93245e19dbf6617ced9
김상우 <kimsangwoo22@gmail.com>
2026-08-25 10:50:11 +0900

    데이터베이스 CRUD 연습용 코드 추가

--- workspace_python/03_database/03_crud.py
+++ workspace_python/03_database/03_crud.py
@@ -23,7 +23,6 @@ def get_session():
 def on_startup() :
     SQLModel.metadata.create_all(engine)
 
-
 # EMP 테이블 전체 조회 페이지 
 @app.get('/list')
 def emp_list(request: Request, session: Session = Depends(get_session)):
@@ -79,7 +78,75 @@ def emp_add_form (
     except Exception as e :
         print(e)
 
+#추가 완료 후 전체 페이지로 리다이렉트
     return RedirectResponse(url='/list' , status_code=303)
+
+# 상세 정보 조회 라우트
+@app.get('/detail/{empno}')
+def detail_page(empno:int, request: Request ,  session: Session = Depends(get_session)):
+    print('/datail/{empno} 실행,  empno:' , empno)
+    sql = text('''
+        select * from emp3
+        where empno = :empno
+         ''')
+    result = session.execute(sql, {"empno" : empno}).mappings().first()
+    return templates.TemplateResponse(request, 'detail.html',{
+            'request' : request,
+            'emp' : result
+        }) 
+
+# 수정 화면 이동 라우트
+@app.get('/update/{empno}')
+def update_page(empno:int, request: Request, session: Session = Depends(get_session)):
+    print('/update/{empno} 실행 , empno:', empno )
+    sql = text ('''
+        select * from emp3
+        where empno = :empno
+    ''')
+    result = session.execute(sql, {"empno" : empno }).mappings().first()
+    return templates.TemplateResponse(request,'update.html', {
+        'request' : request,
+        'emp' : result
+    })
+
+# 테이블 수정처리 API 라우트 
+@app.post('/api/update')
+def emp_update_form(
+    request: Request,  emp: Emp3 = Form(), session: Session = Depends(get_session)
+):
+    try:
+        sql = text('''
+            update emp3
+            set 
+                ename = :ename,
+                job = :job,
+                mgr = :mgr,
+                hiredate = :hiredate,
+                sal = :sal,
+                comm = :comm,
+                deptno = :deptno
+            where 
+                empno = :empno
+        ''')
+        session.execute(sql, {
+            'empno' : Emp3.empno,
+            'ename' : Emp3.ename,
+            'job' : Emp3.job,
+            'mgr' : Emp3.mgr,
+            'hiredate' : Emp3.hiredate,
+            'sal' : Emp3.sal,
+            'comm' : Emp3.comm,
+            'deptno' : Emp3.deptno
+        })
+        
+        session.commit()
+
+    except Exception as e :
+        print(e)
+
+    # 수정 완료 후 전체 조회 페이지로 리다이렉트
+    return RedirectResponse(url='/list' , status_code=303)
+
 
 if __name__ == "__main__":
     import uvicorn
