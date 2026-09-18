613931c5ffbc8dd608318965afe2d28d7cc5a18f
김상우 <kimsangwoo22@gmail.com>
2026-09-18 15:47:58 +0900

    07_elastic/api.py es_router.py를 연결하는 코드를 추가했습니다

--- workspace_python/07_elastic/api.py
+++ workspace_python/07_elastic/api.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
+from router.es_router import router as es_router
 
 app = FastAPI()
+app.include_router(es_router)
 
 @app.get('/')
 def home():
     return "welcome!"
-
-
 
 if __name__ == '__main__' :
     import uvicorn 
